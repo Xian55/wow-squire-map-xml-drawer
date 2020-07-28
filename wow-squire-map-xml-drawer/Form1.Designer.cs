0e89334edeecdd8ec1c85b4139b568af47c0c5b0
Xian55 <polereczkrisz@gmail.com>
2020-07-28 21:16:06 +0200

    Added to VC

---
+++ wow-squire-map-xml-drawer/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace wow_squire_map_xml_drawer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBoxZones = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1. Open xml";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBoxZones);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(93, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(297, 78);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboBoxZones
+            // 
+            this.comboBoxZones.FormattingEnabled = true;
+            this.comboBoxZones.Location = new System.Drawing.Point(55, 21);
+            this.comboBoxZones.Name = "comboBoxZones";
+            this.comboBoxZones.Size = new System.Drawing.Size(230, 21);
+            this.comboBoxZones.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(230, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "3. Generate Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2. Zone";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Generate Image";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.richTextBox);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(93, 96);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(297, 87);
+            this.panel2.TabIndex = 2;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(8, 20);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(277, 60);
+            this.richTextBox.TabIndex = 4;
+            this.richTextBox.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Get Waypoints as macro (tom tom)";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(12, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(78, 171);
+            this.panel3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 189);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Profie Image Gen";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ComboBox comboBoxZones;
+    }
+}
+
---
+++ wow-squire-map-xml-drawer/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace wow_squire_map_xml_drawer
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+
+        private List<PointF> allWaypoints = new List<PointF>();
+
+        private List<PointF> routeWaypoints = new List<PointF>();
+        private List<PointF> ghostWaypoints = new List<PointF>();
+        private List<PointF> vendorWaypoints = new List<PointF>();
+
+        private Bitmap background;
+        private RectangleF boundsRectangle;
+
+        private string inputFileName;
+
+        private const float MapScalar = 100f;
+        private const int enlarge = 40;
+
+        #endregion
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            PopulateZoneCombobox();
+        }
+
+
+        #region Profile File
+
+        private void OpenProfileFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = Path.GetDirectoryName(Directory.GetCurrentDirectory())
+            };
+            openFileDialog.ShowDialog();
+
+            if (string.IsNullOrEmpty(openFileDialog.FileName))
+                return;
+
+            inputFileName = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+
+            ProcessProfileFile(openFileDialog.FileName);
+        }
+
+        private void ProcessProfileFile(string inputFile)
+        {
+            Cleanup();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(inputFile);
+
+            PopulateWayPointList(doc, "Grind/Waypoints/Normal", ref routeWaypoints, true);
+            PopulateWayPointList(doc, "Grind/Waypoints/Ghost", ref ghostWaypoints);
+            PopulateWayPointList(doc, "Grind/Waypoints/Vendor", ref vendorWaypoints);
+        }
+
+        private void PopulateWayPointList(XmlDocument doc, string xpath, ref List<PointF> list, bool showAddon=false)
+        {
+            XmlNode current = doc.FirstChild.SelectSingleNode(xpath);
+            if (current == null)
+            {
+                return;
+            }
+
+            foreach (XmlNode child in current.ChildNodes)
+            {
+                float x = float.Parse(child.Attributes.GetNamedItem("X").Value, CultureInfo.InvariantCulture);
+                float y = float.Parse(child.Attributes.GetNamedItem("Y").Value, CultureInfo.InvariantCulture);
+
+                if (showAddon)
+                {
+                    richTextBox.Text += $"/way {x} {y}\n";
+                }
+
+                list.Add(new PointF(x / MapScalar, y / MapScalar));
+                allWaypoints.Add(new PointF(x / MapScalar, y / MapScalar));
+
+                boundsRectangle = RecalculateBounds(ref allWaypoints);
+            }
+        }
+
+        #endregion
+
+
+        #region Drawing
+
+        private Bitmap DownloadImageAsBitmap(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return null;
+
+            using (WebClient client = new WebClient())
+            {
+                byte[] imageBytes = client.DownloadData(url);
+                using (var ms = new MemoryStream(imageBytes))
+                {
+                    return new Bitmap(ms);
+                }
+            }
+        }
+
+        private Rectangle Draw()
+        {
+            float width = (float)background.Width;
+            float height = (float)background.Height;
+            float radius = 3;
+
+            using (Graphics gr = Graphics.FromImage(background))
+            {
+                DrawPath(gr, ghostWaypoints, width, height, radius, Brushes.Black, Brushes.Aqua);
+                DrawPath(gr, vendorWaypoints, width, height, radius, Brushes.Orange, Brushes.Yellow);
+                DrawPath(gr, routeWaypoints, width, height, radius, Brushes.White, Brushes.Red);
+
+                RectangleF r = boundsRectangle;
+                r.X *= width;
+                r.Y *= height;
+                r.Width *= width;
+                r.Height *= height;
+
+                r.Inflate(enlarge, enlarge);
+
+                if (r.X < 0)
+                    r.X = 0;
+                if (r.Y < 0)
+                    r.Y = 0;
+
+                // Draw starting pos to the bottom
+                Rectangle boundingbox = Rectangle.Round(r);
+
+                // Black background
+                PointF startPoint = routeWaypoints.Last();
+                string startText = $"{startPoint.X * MapScalar} {startPoint.Y * MapScalar}";
+                var sizeStart = gr.MeasureString(startText, Font);
+
+                Rectangle infoRect = new Rectangle(new Point(boundingbox.X, boundingbox.Bottom), new Size(boundingbox.Width, (int)sizeStart.Height));
+                gr.FillRectangle(Brushes.Black, infoRect);
+
+                // Start: text
+                Rectangle startRect = new Rectangle(new Point(infoRect.X, infoRect.Y), new Size((int)(sizeStart.Width+1), infoRect.Height));
+                gr.DrawString(startText, Font, Brushes.White, startRect);
+
+                
+                // Draw if it has ghost waypoints
+                Rectangle ghostRect = startRect;
+                ghostRect.Offset(startRect.Width + 1, 0);
+                if (ghostWaypoints.Count > 0)
+                {
+                    gr.DrawString("G", Font, Brushes.Aqua, ghostRect);
+                }
+
+                // Draw if it has vendor waypoints
+                Rectangle vendorRect = ghostRect;
+                vendorRect.Offset(10, 0);
+                if (vendorWaypoints.Count > 0)
+                {
+                    gr.DrawString("V", Font, Brushes.Yellow, vendorRect);
+                }
+
+                Rectangle outputRect = boundingbox;
+                outputRect.Height += infoRect.Height;
+                return outputRect;
+            }
+        }
+
+        private void DrawPath(Graphics gr, List<PointF> list, float width, float height, float radius, Brush first, Brush others)
+        {
+            // reverse it so first drawn last
+            list.Reverse();
+            list.ForEach((p) =>
+            {
+                gr.FillEllipse(others, width * p.X, height * p.Y, radius, radius);
+            });
+
+            if(list.Any())
+            {
+                var pStart = list.Last();
+                gr.FillEllipse(first, width * pStart.X, height * pStart.Y, radius, radius);
+            }
+        }
+
+        #endregion
+
+
+        #region Util 
+
+        private RectangleF RecalculateBounds(ref List<PointF> list)
+        {
+            var minX = list.Min(p => p.X);
+            var minY = list.Min(p => p.Y);
+            var maxX = list.Max(p => p.X);
+            var maxY = list.Max(p => p.Y);
+
+            return new RectangleF(new PointF(minX, minY), new SizeF(maxX - minX, maxY - minY));
+        }
+
+        private void SaveBitmap(Bitmap bitmap, string name)
+        {
+            var b = new Bitmap(bitmap);
+
+            var encoder = ImageCodecInfo.GetImageEncoders().First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+            var encParams = new EncoderParameters() { Param = new[] { new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 100L) } };
+
+            b.Save(name, encoder, encParams);
+        }
+
+        #endregion
+
+
+        #region WinForm
+        private void PopulateZoneCombobox()
+        {
+            comboBoxZones.Items.Clear();
+
+            foreach (var entry in ZoneMapDict.List)
+            {
+                comboBoxZones.Items.Add(entry.Key);
+            }
+        }
+
+        private void Cleanup()
+        {
+            richTextBox.Text = string.Empty;
+
+            routeWaypoints.Clear();
+            ghostWaypoints.Clear();
+            vendorWaypoints.Clear();
+            allWaypoints.Clear();
+
+            if (background != null)
+            {
+                background.Dispose();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenProfileFile();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(comboBoxZones.SelectedItem == null || string.IsNullOrEmpty(comboBoxZones.SelectedItem.ToString()))
+            {
+                return;
+            }
+
+            background = DownloadImageAsBitmap(ZoneMapDict.GetUrl(comboBoxZones.SelectedItem.ToString()));
+            if (background != null)
+            {
+                Rectangle outputRect = Draw();
+                Bitmap output = background.Clone(outputRect, PixelFormat.Format32bppArgb);
+                SaveBitmap(output, inputFileName + ".jpg");
+            }
+            else
+            {
+                MessageBox.Show("ImageURL is null");
+            }
+        }
+
+        #endregion
+    }
+}
